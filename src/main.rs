142368a214391591df6425a59518d2244a41d27b
kash gohil <kashyapgohil476@gmail.com>
2026-05-30 13:42:41 +0530

    Initial commit to render block wth ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::{
+    crossterm,
+    widgets::{Block, BorderType, Borders},
+};
+
+fn main() -> std::io::Result<()> {
+    let mut terminal = ratatui::init();
+
+    loop {
+        terminal.draw(|frame| {
+            let block = Block::default()
+                .borders(Borders::ALL)
+                .title("Scrolls")
+                .border_type(BorderType::Rounded);
+
+            frame.render_widget(block, frame.area());
+        })?;
+
+        if let crossterm::event::Event::Key(key) = crossterm::event::read()? {
+            if key.code == crossterm::event::KeyCode::Char('q') {
+                break;
+            }
+        }
+    }
+
+    ratatui::restore();
+    Ok(())
 }
